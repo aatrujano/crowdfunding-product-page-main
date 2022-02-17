2fb532688e8601aad884da7251b7fef6b495c78d
aatrujano <76922673+aatrujano@users.noreply.github.com>
2022-02-16 18:59:28 -0600

    About section responsive

--- js/main.js
+++ js/main.js
@@ -2,9 +2,6 @@
 let bookmarked = false;
 const bookmark = document.getElementById('bookmarkContainer');
 
-console.log(bookmark.innerHTML);
-console.log(svg);
-
 const toggleBookmark = () => {
   bookmarked = !bookmarked;
   bookmark.classList.toggle('bookmarked');
@@ -16,3 +13,22 @@ const toggleBookmark = () => {
 };
 
 bookmark.addEventListener('click', toggleBookmark);
+
+// CHECK IF THERE ARE ITEMS OUT OF STOCK
+
+const checkForOutOfStockItems = () => {
+  const rewardContainers = document.querySelectorAll(
+    '.main__about-rewardContainer'
+  );
+  const rewards = document.querySelectorAll('.rewardInfoAndCta');
+
+  rewards.forEach((reward, index) => {
+    if (reward.children[0].children[0].innerHTML === '0') {
+      reward.children[1].disabled = true;
+      reward.children[1].innerHTML = 'Out of stock';
+      rewardContainers[index].classList.add('outOfStock');
+    }
+  });
+};
+
+checkForOutOfStockItems();
