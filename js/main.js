fbbad7a6358ada439d1af974e0f7614811ba5a26
aatrujano <76922673+aatrujano@users.noreply.github.com>
2022-02-19 14:11:11 -0600

    backThisProject button functionality

--- js/main.js
+++ js/main.js
@@ -1,21 +1,4 @@
-// BOOKMARK BUTTON
-let bookmarked = false;
-const bookmark = document.getElementById('bookmarkContainer');
-
-const toggleBookmark = () => {
-  bookmarked = !bookmarked;
-  bookmark.classList.toggle('bookmarked');
-  if (bookmarked) {
-    bookmarkButton.innerHTML = 'Bookmarked';
-  } else {
-    bookmarkButton.innerHTML = 'Bookmark';
-  }
-};
-
-bookmark.addEventListener('click', toggleBookmark);
-
 // CHECK IF THERE ARE ITEMS OUT OF STOCK
-
 const checkForOutOfStockItems = () => {
   const rewardContainers = document.querySelectorAll(
     '.main__about-rewardContainer'
@@ -32,3 +15,45 @@ const checkForOutOfStockItems = () => {
 };
 
 checkForOutOfStockItems();
+
+// BOOKMARK BUTTON
+let bookmarked = false;
+const bookmark = document.getElementById('bookmarkContainer');
+
+const toggleBookmark = () => {
+  bookmarked = !bookmarked;
+  bookmark.classList.toggle('bookmarked');
+  if (bookmarked) {
+    bookmarkButton.innerHTML = 'Bookmarked';
+  } else {
+    bookmarkButton.innerHTML = 'Bookmark';
+  }
+};
+
+bookmark.addEventListener('click', toggleBookmark);
+
+// BACK THIS PROJECT BUTTON
+const backProjectButton = document.getElementById('backProject');
+const backProjectModal = document.getElementById('backThisProject');
+const overlay = document.getElementById('overlay');
+
+const showBackThisProjectModal = () => {
+  backProjectModal.classList.remove('removed');
+  backProjectModal.classList.add('showModal');
+  overlay.classList.remove('hidden');
+  overlay.classList.add('shown');
+  console.log('backThisButton clicked')
+}
+
+backProjectButton.addEventListener('click', showBackThisProjectModal);
+
+//CLOSE MODAL
+const closeModalIcon = document.getElementById('closeModalIcon');
+ const closeBackThisProjectModal = () => {
+  backProjectModal.classList.remove('showModal');
+  backProjectModal.classList.add('removed');
+  overlay.classList.remove('shown');
+  overlay.classList.add('hidden');
+ }
+
+closeModalIcon.addEventListener('click', closeBackThisProjectModal);
